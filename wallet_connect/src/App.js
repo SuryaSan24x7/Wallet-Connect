00b543ebde9cdde60d4a05410775935f787a535c
Suraj Gyaneshwar Waghmare <56404608+SuryaSan24x7@users.noreply.github.com>
2024-09-26 14:07:36 +0530

    Update App.js

--- wallet_connect/src/App.js
+++ wallet_connect/src/App.js
@@ -13,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/phantom-wallet" element={<Phantom />} />
+        <Route path="/contract-connection" element={<ContractConnection />} />
       </Routes>
     </BrowserRouter>
       <Analytics />
